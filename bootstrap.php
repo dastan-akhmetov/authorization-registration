c58db3c5cf159cfc8863a7a3ba72f27aae3021ef
Dastan Akhmetov <dastan.akhmetov@nurotan.kz>
2016-10-04 18:53:55 +0600

    Created a bootstrap.php. Made a single point of Database class inclusion. Declared a BASE_URL variable.

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dastan
+ * Date: 10/4/16
+ * Time: 4:32 PM
+ */
+
+require_once "classes/Database.php";
+
+$BASE_URL = "http://".$_SERVER["SERVER_NAME"]."/";
