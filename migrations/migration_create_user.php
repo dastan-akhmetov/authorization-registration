593be7315b97c83a2effa446c75f0da376130fa6
Dastan Akhmetov <dastan.akhmetov@nurotan.kz>
2016-10-04 16:15:02 +0600

    Created a migration for user table creation.

---
+++ migrations/migration_create_user.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dastan
+ * Date: 10/4/16
+ * Time: 3:55 PM
+ */
+
+require "../classes/Database.php";
+
+function create()
+{
+
+    // instantiate an object
+    $db = new Database();
+
+    // cleanup
+    $sql = "DROP TABLE IF EXISTS `user`";
+    $db->executeNoParams($sql);
+
+    // create a table
+    $sql = "CREATE TABLE `user`(
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `username` VARCHAR (255) NOT NULL,
+      `password` VARCHAR (255) NOT NULL,
+      `firstname` VARCHAR (255) NOT NULL,
+      `lastname` VARCHAR (255) NOT NULL,
+      `date_of_birth` DATE NOT NULL,
+      `gender` VARCHAR (6) NOT NULL,
+      `email` VARCHAR (255) NOT NULL,
+      PRIMARY KEY (`id`),
+      INDEX `username` (`username` ASC),
+      INDEX `email` (`email` ASC),
+    )";
+    $db->executeNoParams($sql);
+
+}
+
+create();
