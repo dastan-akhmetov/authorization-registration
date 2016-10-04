be48ff817c7d3bddd42887634282238e1bdfccd8
Dastan Akhmetov <dastan.akhmetov@nurotan.kz>
2016-10-04 18:55:26 +0600

    Created a User class.

---
+++ classes/User.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: dastan
+ * Date: 10/4/16
+ * Time: 4:23 PM
+ */
+class User
+{
+    
+    public function __construct()
+    {
+        
+        
+        
+    }
+
+}
