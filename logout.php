9da6b27ab0351572488d8e27b9fff41a4b0e680f
Dastan Akhmetov <dastan.akhmetov@nurotan.kz>
2016-10-04 18:56:05 +0600

    Created a logout.php page.

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dastan
+ * Date: 10/4/16
+ * Time: 4:28 PM
+ */
+require_once "templates/header.php";
+?>
+
+<a href="index.php">Index</a>
+
+<?php
+require_once "templates/footer.php";
+?>
