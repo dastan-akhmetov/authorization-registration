12ace6098595072f1842d022aa79b4ea7f0ce161
Dastan Akhmetov <dastan.akhmetov@nurotan.kz>
2016-10-04 18:52:33 +0600

    Added a templates and menu of unordered lists

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-include_once "db.php";
-$db = new Database();
+require_once "templates/header.php";
+?>
+
+<ul>
+    <?php
+        if (isset($_SESSION["logged_in"]))
+            echo "<li><a href=\"logout.php\">Logout</a></li>";
+        else {
+            echo "<li><a href=\"login.php\">Login</a></li>";
+            echo "<li><a href=\"register.php\">Register</a></li>";
+        }
+    ?>
+</ul>
+
+
+
+
+
+<?php
+require_once "templates/footer.php.php";
+?>
