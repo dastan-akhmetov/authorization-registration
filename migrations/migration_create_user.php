38e568b0a285a8571b102e3d1ccb33fbf530bb78
Dastan Akhmetov <dastan.akhmetov@nurotan.kz>
2016-10-04 17:36:06 +0600

    Deleted a username column in migration_create_user.php

--- migrations/migration_create_user.php
+++ migrations/migration_create_user.php
@@ -5,9 +5,12 @@
  * Date: 10/4/16
  * Time: 3:55 PM
  */
-ini_set('display_errors', 1);
+
 require "../classes/Database.php";
 
+/**
+ * The function creates a table `User`
+ */
 function create()
 {
 
@@ -16,23 +19,21 @@ function create()
 
     // cleanup
     $sql = "DROP TABLE IF EXISTS `user`";
-    $db->executeNoParams($sql);
+    $db->queryNoParams($sql);
 
     // create a table
     $sql = "CREATE TABLE `user`(
       `id` INT NOT NULL AUTO_INCREMENT,
-      `username` VARCHAR (255) NOT NULL,
+      `email` VARCHAR (255) NOT NULL,
       `password` VARCHAR (255) NOT NULL,
       `firstname` VARCHAR (255) NOT NULL,
       `lastname` VARCHAR (255) NOT NULL,
       `date_of_birth` DATE NOT NULL,
       `gender` VARCHAR (6) NOT NULL,
-      `email` VARCHAR (255) NOT NULL,
       PRIMARY KEY (`id`),
-      INDEX `username` (`username` ASC),
       INDEX `email` (`email` ASC)
     )";
-    $db->executeNoParams($sql);
+    $db->queryNoParams($sql);
 
 }
 
