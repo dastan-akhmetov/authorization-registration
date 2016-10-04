e131a8980d8d4b3aa41ea4ed58604e50d67624b1
Dastan Akhmetov <dastan.akhmetov@nurotan.kz>
2016-10-04 18:56:37 +0600

    Created a header.php template file.

---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dastan
+ * Date: 10/4/16
+ * Time: 4:26 PM
+ */
+require_once "bootstrap.php";
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title></title>
+        <link type="text/css" rel="stylesheet" href="<?php echo $BASE_URL; ?>assets/css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="<?php echo $BASE_URL; ?>assets/css/styles.css">
+    </head>
+    <body>
+        <div class="container">
