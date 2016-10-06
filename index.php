bbd9e9f26ef83fb60191c1516b6eea23346dea8d
Dastan Akhmetov <ddakhmet@gmail.com>
2016-10-06 19:26:35 +0600

    Cleaned up from unnecessary stuff

--- index.php
+++ index.php
@@ -2,22 +2,7 @@
 require_once "templates/header.php";
 ?>
 
-<ul>
-    <?php
-        if (isset($_SESSION["logged_in"]))
-            render_link("logout");
-        else {
-            echo "<ul>";
-                echo "<li>";
-                    render_link("authorization");
-                echo "</li>";
-                echo "<li>";
-                    render_link("registration");
-                echo "</li>";
-            echo "</ul>";
-        }
-    ?>
-</ul>
+
 
 
 
