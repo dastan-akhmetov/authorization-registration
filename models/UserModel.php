bb64aa11f4cc32f05e3cdc33202fa00417281148
Dastan Akhmetov <ddakhmet@gmail.com>
2016-10-07 18:14:07 +0600

    Refactored to extend DatabaseModel.php

--- models/UserModel.php
+++ models/UserModel.php
@@ -1,21 +1,18 @@
 <?php
 
-namespace Models\User;
-
 /**
  * Created by PhpStorm.
  * User: dastan
  * Date: 10/4/16
  * Time: 4:23 PM
  */
-class User extends Database
+class UserModel extends DatabaseModel
 {
     
     public function __construct()
     {
         
         parent::__construct();
-        echo "This is User Model";
         
     }
 
