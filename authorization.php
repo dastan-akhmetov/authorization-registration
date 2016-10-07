5841a9d319ebddf9bdd4b6bd654acdc8cb01f9ff
Dastan Akhmetov <ddakhmet@gmail.com>
2016-10-07 18:12:00 +0600

    Refactored code for better validation

--- authorization.php
+++ authorization.php
@@ -7,18 +7,43 @@
  */
 include_once "templates/header.php";
 ?>
-    <form class="form-signin">
+    <?php
+        if (isset($_POST["submitButton"])) {
+
+            $user = new UserController();
+            $user->TITLES = $VALIDATION;
+
+            $user->email = $_POST["inputEmail"];
+            $user->password = $_POST["inputPassword"];
+
+            $user->validate_fields();
+        }
+
+    ?>
+
+    <form class="form-signin" method="POST" action="<?= $CURRENT_PAGE ?>">
+
+        <!-- Legend -->
         <legend><?php render_title(); ?></legend>
+
+        <!-- Email -->
         <label for="inputEmail" class="sr-only"><?= $TITLES["email"] ?></label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="<?= $TITLES["email"] ?>" required autofocus>
+        <input type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="<?= $TITLES["email"] ?>" value="<?php render_input_value("inputEmail"); ?>" required autofocus>
+
+        <!-- Password -->
         <label for="inputPassword" class="sr-only"><?= $TITLES["password"] ?></label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="<?= $TITLES["password"] ?>" required>
+        <input type="password" name="inputPassword" id="inputPassword" class="form-control" placeholder="<?= $TITLES["password"] ?>" value="<?php render_input_value("inputPassword"); ?>" required>
+
+        <!-- Remember me -->
         <div class="checkbox">
             <label>
                 <input type="checkbox" value="remember-me"> <?= $TITLES["remember_me"] ?>
             </label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit"><?= $TITLES["sign_in"] ?></button>
+
+        <!-- Submit Button -->
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="submitButton"><?= $TITLES["sign_in"] ?></button>
+
     </form>
 
 <?php
