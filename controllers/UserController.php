9c4bab46e851a2b2b895cc69c6860b8ee6584032
Dastan Akhmetov <ddakhmet@gmail.com>
2016-10-09 19:37:37 +0600

    Refactored code Added methods for photo uploading, photo validation Duplicate error for registration

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -9,19 +9,28 @@
 class UserController
 {
 
+
     private $model;
     private $TITLES;
     private $REGISTRATION_TITLES;
+    private $AUTHORIZATION_TITLES;
     private $email;
     private $password;
     private $password_repeat;
     private $firstname;
     private $lastname;
+    private $photo;
+    private $photo_url;
+    private $photo_type;
     private $day_of_birth;
     private $month_of_birth;
     private $year_of_birth;
     private $gender;
     private $isRegistering;
+
+    private $FILE_UPLOAD_SIZE;
+    private $ABS_UPLOAD_DIR;
+    private $SHORT_UPLOAD_DIR;
 
     private $message;
     private $message_type;
@@ -29,9 +38,17 @@ class UserController
 
     public function __construct()
     {
+        global $FILE_UPLOAD_SIZE;
+        global $ABS_UPLOAD_DIR;
+        global $SHORT_UPLOAD_DIR;
 
         $this->model = new UserModel;
+        $this->remember_me = FALSE;
         $this->isRegistering = FALSE;
+
+        $this->FILE_UPLOAD_SIZE = $FILE_UPLOAD_SIZE;
+        $this->ABS_UPLOAD_DIR = $ABS_UPLOAD_DIR;
+        $this->SHORT_UPLOAD_DIR = $SHORT_UPLOAD_DIR;
 
     }
 
@@ -55,9 +72,8 @@ class UserController
 
     }
 
-    public function register()
+    public function authorize()
     {
-
         $validations_ok = $this->validate_fields();
 
         if ($validations_ok) {
@@ -66,14 +82,87 @@ class UserController
 
             $this->model->email = $this->email;
             $this->model->password = $hash_password;
+
+            $authorize = $this->model->authorize();
+
+            if (count($authorize) < 1) {
+                $this->message_type = "danger";
+                $this->message = $this->AUTHORIZATION_TITLES["credentials_dont_match"];
+
+                $this->print_message();
+
+                return FALSE;
+            }
+
+            $user = $authorize[0];
+
+            if ($user["password"] == $hash_password && $user["email"] == $this->email) {
+
+                $this->message_type = "success";
+                $this->message = $this->AUTHORIZATION_TITLES["successful_authorization"];
+
+                $this->print_message();
+
+                $_SESSION["logged_in"] = TRUE;
+                $_SESSION["firstname"] = $user["firstname"];
+                $_SESSION["lastname"] = $user["lastname"];
+                $_SESSION["photo_url"] = $user["photo_url"];
+                $_SESSION["date_of_birth"] = $user["date_of_birth"];
+                $_SESSION["gender"] = $user["gender"];
+                $_SESSION["email"] = $user["email"];
+
+                return TRUE;
+
+            }
+            else {
+
+                $this->message_type = "danger";
+                $this->message = $this->AUTHORIZATION_TITLES["credentials_dont_match"];
+
+                $this->print_message();
+
+                return FALSE;
+
+            }
+
+
+        }
+    }
+
+    public function register()
+    {
+
+        $validations_ok = $this->validate_fields();
+
+        if ($validations_ok === TRUE) {
+
+            $upload_photo = $this->upload_photo();
+
+            if (!$upload_photo) {
+
+                $this->message_type = "danger";
+                $this->message = $this->REGISTRATION_TITLES["file_cannot_be_uploaded"];
+
+                $this->print_message();
+
+                return FALSE;
+
+            }
+
+            $hash_password = sha1($this->password);
+
+            $this->model->email = $this->email;
+            $this->model->password = $hash_password;
             $this->model->firstname = $this->firstname;
             $this->model->lastname = $this->lastname;
+            $this->model->photo_url = $this->photo_url;
+
             $this->model->gender = $this->gender;
             $this->model->date_of_birth = $this->year_of_birth . '-' . $this->month_of_birth . '-' . $this->day_of_birth;
 
             $register = $this->model->register();
 
-            if ($register) {
+            if ($register === TRUE) {
 
                 $this->message_type = "success";
                 $this->message = $this->REGISTRATION_TITLES["successful_registration"];
@@ -86,7 +175,14 @@ class UserController
             else {
 
                 $this->message_type = "danger";
-                $this->message = $this->REGISTRATION_TITLES["failed_registration"];
+
+                if ($register === "Duplicate")
+
+                    $this->message = $this->REGISTRATION_TITLES["duplicate_email"];
+
+                else
+
+                    $this->message = $this->REGISTRATION_TITLES["failed_registration"];
 
                 $this->print_message();
 
@@ -105,6 +201,28 @@ class UserController
             echo "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>";
             echo $this->message;
         echo "</div>";
+
+    }
+
+    public function upload_photo()
+    {
+
+        $upload_filename = $this->photo["name"];
+        $upload_filename = hash("sha256", $upload_filename . time());
+
+        $photo_url = $this->ABS_UPLOAD_DIR . $upload_filename . "." . $this->photo_type;
+        $this->photo_url = $this->SHORT_UPLOAD_DIR . $upload_filename . "." . $this->photo_type;
+
+        if (move_uploaded_file($this->photo["tmp_name"], $photo_url)) {
+
+            return TRUE;
+
+        }
+        else {
+
+            return FALSE;
+
+        }
 
     }
 
@@ -161,6 +279,15 @@ class UserController
             foreach ($lastnames as $lastname) {
 
                 $messages[] = $lastname;
+
+            }
+
+            // Validate photo
+            $photos = $this->validate_photo();
+
+            foreach ($photos as $photo) {
+
+                $messages[] = $photo;
 
             }
 
@@ -368,6 +495,87 @@ class UserController
             $message[] = FALSE;
 
         return $message;
+
+    }
+
+    public function validate_photo()
+    {
+
+        $message = [];
+
+        $error = $this->photo["error"];
+
+        $format = $this->validate_photo_format();
+
+        $size = $this->validate_photo_size();
+
+        if ($error === 0) {
+
+            if ($format) {
+
+                if ($size)
+
+                    $message[] = FALSE;
+
+                else // size
+
+                    $message[] = $this->TITLES["file_is_bigger"];
+
+            } // format
+            else
+
+                $message[] = $this->TITLES["file_format_incorrect"];
+
+        } // error
+        else
+
+            $message[] = $this->TITLES["file_cannot_be_uploaded"] . " " . $error;
+
+        return $message;
+
+    }
+
+    public function validate_photo_format()
+    {
+
+        $allowed_formats = [
+            "image/gif",
+            "image/jpg",
+            "image/jpeg",
+            "image/png"
+        ];
+
+        $format = $this->photo["type"];
+        $type = array_search($format, $allowed_formats);
+
+        if ($type) {
+
+            if ($format == "image/jpeg")
+                $format = "image/jpg";
+
+            $this->photo_type = str_replace("image/", "", $format);
+
+            return TRUE;
+
+        }
+        else
+
+            return FALSE;
+
+    }
+
+    public function validate_photo_size()
+    {
+
+        $size = $this->photo["size"];
+
+        if ($size <= $this->FILE_UPLOAD_SIZE)
+
+            return TRUE;
+
+        else
+
+            return FALSE;
 
     }
 
