8a2ead5c60b2f9aa0352f02b487bf3be7e863b60
Dastan Akhmetov <ddakhmet@gmail.com>
2016-10-11 04:29:08 +0600

    Created a page for ajax calls

---
+++ ajax.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hoph
+ * Date: 10/11/16
+ * Time: 3:31 AM
+ */
+if (isset($_POST["action"])) {
+
+    require_once 'bootstrap.php';
+
+    $user = new UserController();
+
+    if ($_POST["action"] == "check_email") {
+
+        $user->email = $_POST["email"];
+        $isDuplicate = $user->check_email_duplicate();
+
+        $response = [
+            'isDuplicate' => $isDuplicate
+        ];
+
+        echo json_encode($response);
+        exit();
+
+    }
+
+    if ($_POST["action"] == "change_password") {
+
+        if ($_POST["password"] == $_POST["password_repeat"]) {
+
+            $user->email = $_POST["email"];
+            $user->password = $_POST["password"];
+
+            $result = $user->change_password();
+
+            $response = [
+                'passwordChanged' => $result
+            ];
+
+        }
+        else {
+
+            $response = [
+                'passwordChanged' => FALSE,
+                'reason' => 'passwords_do_not_match'
+            ];
+
+        }
+
+        echo json_encode($response);
+        exit();
+
+    }
+
+
+
+}
