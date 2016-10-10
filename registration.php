a175b45c08c899b0839900bc528c206d0a514338
Dastan Akhmetov <ddakhmet@gmail.com>
2016-10-11 01:32:08 +0600

    Added some additional markup for validation

--- registration.php
+++ registration.php
@@ -55,47 +55,52 @@ require_once "templates/header.php";
             <!-- Email -->
             <div class="form-group">
                 <label for="inputEmail" class="col-lg-4 control-label"><?= $TITLES["email"] ?></label>
-                <div class="col-lg-8">
+                <div class="col-lg-5">
                     <input type="text" name="inputEmail" class="form-control" id="inputEmail" placeholder="<?= $TITLES["email"] ?>" value="<?php render_input_value("inputEmail", $registered); ?>" required autofocus>
                 </div>
+                <span id="inputEmailHint" class="hints control-label col-lg-3"></span>
             </div>
 
             <!-- Password -->
             <div class="form-group">
                 <label for="inputPassword" class="col-lg-4 control-label"><?= $TITLES["password"] ?></label>
-                <div class="col-lg-8">
+                <div class="col-lg-5">
                     <input type="password" name="inputPassword" class="form-control" id="inputPassword" placeholder="<?= $TITLES["password"] ?>" value="<?php render_input_value("inputPassword", $registered); ?>" required>
                 </div>
+                <span id="inputPasswordHint" class="hints control-label col-lg-3"></span>
             </div>
 
             <!-- Repeat Password -->
             <div class="form-group">
                 <label for="inputPasswordRepeat" class="col-lg-4 control-label"><?= $TITLES["password_repeat"] ?></label>
-                <div class="col-lg-8">
+                <div class="col-lg-5">
                     <input type="password" name="inputPasswordRepeat" class="form-control" id="inputPasswordRepeat" placeholder="<?= $TITLES["password_repeat"] ?>" value="<?php render_input_value("inputPasswordRepeat", $registered); ?>" required>
                 </div>
+                <span id="inputPasswordRepeatHint" class="hints control-label col-lg-3"></span>
             </div>
 
             <!-- Firstname -->
             <div class="form-group">
                 <label for="inputFirstname" class="col-lg-4 control-label"><?= $TITLES["firstname"] ?></label>
-                <div class="col-lg-8">
+                <div class="col-lg-5">
                     <input type="text" name="inputFirstname" class="form-control" id="inputFirstname" placeholder="<?= $TITLES["firstname"] ?>" value="<?php render_input_value("inputFirstname", $registered); ?>" required>
                 </div>
+                <span id="inputFirstnameHint" class="hints control-label col-lg-3"></span>
             </div>
 
             <!-- Lastname -->
             <div class="form-group">
                 <label for="inputLastname" class="col-lg-4 control-label"><?= $TITLES["lastname"] ?></label>
-                <div class="col-lg-8">
+                <div class="col-lg-5">
                     <input type="text" name="inputLastname" class="form-control" id="inputLastname" placeholder="<?= $TITLES["lastname"] ?>" value="<?php render_input_value("inputLastname", $registered); ?>" required>
                 </div>
+                <span id="inputLastnameHint" class="hints control-label col-lg-3"></span>
             </div>
 
             <!-- Photo -->
             <div class="form-group">
                 <label for="inputPhoto" class="col-lg-4 control-label"><?= $TITLES["photo"] ?></label>
-                <div class="col-lg-8">
+                <div class="col-lg-5">
                     <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
                     <input type="file" name="inputPhoto" class="form-control" id="inputPhoto" />
                 </div>
@@ -104,26 +109,27 @@ require_once "templates/header.php";
             <!-- Gender -->
             <div class="form-group">
                 <label class="col-lg-4 control-label"><?= $TITLES["gender"] ?></label>
-                <div class="col-lg-8">
+                <div class="col-lg-5">
                     <div class="radio">
-                        <label>
+                        <label id="labelGenderMale">
                             <input type="radio" name="inputGender" id="inputGenderMale" value="male" <?php render_radio_checked("inputGender", "male", $registered); ?> required>
                             <?= $TITLES["male"] ?>
                         </label>
                     </div>
                     <div class="radio">
-                        <label>
+                        <label id="labelGenderFemale">
                             <input type="radio" name="inputGender" id="inputGenderFemale" value="female" <?php render_radio_checked("inputGender", "female", $registered); ?> required>
                             <?= $TITLES["female"] ?>
                         </label>
                     </div>
                 </div>
+                <span id="inputGenderHint" class="hints control-label col-lg-3"></span>
             </div>
 
             <!-- Date of Birth -->
             <div class="form-group">
                 <label for="select" class="col-lg-4 control-label"><?= $TITLES["date_of_birth"] ?></label>
-                <div class="col-lg-8 form-dob">
+                <div class="col-lg-5 form-dob">
                     <div class="row">
 
                         <!-- Day -->
@@ -161,13 +167,14 @@ require_once "templates/header.php";
 
                     </div> <!-- .row -->
                 </div>
+                <span id="selectHint" class="hints control-label col-lg-3"></span>
             </div> <!-- Date of Birth -->
 
             <!-- Submit and Clean Buttons -->
             <div class="form-group">
                 <div class="col-lg-10 col-lg-offset-4">
                     <button type="reset" class="btn btn-default"><?= $TITLES["clean"] ?></button>
-                    <button type="submit" name="submitButton" class="btn btn-primary"><?= $TITLES["register"] ?></button>
+                    <button type="submit" name="submitButton" id="submitButton" class="btn btn-primary"><?= $TITLES["register"] ?></button>
                 </div>
             </div>
 
